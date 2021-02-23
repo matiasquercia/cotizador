42bdb39eafaaef008307c7646a0f2ec871ce25ef
Matías Quercia <matias.quercia@gmail.com>
2021-02-23 01:24:45 -0300

    Version1.0

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import Header from './Components/Header';
+import Formulario from './Components/Formulario';
+import Resumen from './Components/Resumen';
+import Resultado from './Components/Resultado';
+import Spinner from './Components/Spinner';
+
+import styled from '@emotion/styled';
+
+const Contenedor =  styled.div `
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario =  styled.div `
+  background-color: #FFF;
+  padding: 3rem;
+`;
+
+function App() {
+
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: ''
+    }
+  });
+
+  const [cargando, guardarCargando] = useState(false);
+
+  //extraer datos
+  const {cotizacion, datos} = resumen;
+
+  return (
+    <Contenedor>
+        <Header
+          titulo='Cotizador de Seguros'
+        />
+     
+
+        <ContenedorFormulario>
+            <Formulario 
+              guardarResumen={guardarResumen}
+              guardarCargando={guardarCargando}
+            />
+            
+            <Resumen 
+              datos={datos}
+            /> 
+
+            {cargando ? <Spinner /> : null}
+
+            { !cargando ?
+              <Resultado 
+                cotizacion={cotizacion}
+              /> : null
+            }         
+         
+        </ContenedorFormulario>
+
+     </Contenedor>
+  );
+}
+
+export default App;
